a4b640499c86c4e945ef6e476da35a0d7fc3c57c
215eight <215eight@gmail.com>
2021-03-03 07:22:43 -0800

    Update package files to use https insteado of ssh

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "git@github.com:215eight/SEPlatform.git", .branch("main")),
+        .package(url: "https://github.com/215eight/SEPlatform.git", .branch("main")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
